f4602bea9e09ab66128abaefec8bc0e45b7af289
Moustapha Hakim <m.hakim@advantive.net>
2023-10-20 21:54:58 +0300

    music picker

---
+++ app/src/main/java/com/example/desh/presentation/view/CoverImageView.kt
@@ -0,0 +1,25 @@
+package com.example.desh.presentation.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+import com.example.desh.databinding.LayoutChooseCoverViewBinding
+
+class CoverImageView : RelativeLayout {
+
+    private lateinit var binding: LayoutChooseCoverViewBinding
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context, attrs, defStyle
+    )
+
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        binding = LayoutChooseCoverViewBinding.inflate(
+            android.view.LayoutInflater.from(context), this, true
+        )
+    }
+
+}
